2b2d257d7f13851569b4b6f8b27d58139d51392f
Roberto Nicoletti <roberto.urias@gmail.com>
2020-06-02 23:16:53 -0300

    Add endpoints ao node

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,49 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuário');
+// Rota: endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
 
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Roberto'
-    ]);
-})
+// GET: Buscar uma ou mais informações no back-end
+// POST: Criar umna nova informação no back-end
+// PUT: Atualizar uma informação exsitente no back-end
+// DELETE: Remover uma informação do back-end
+
+// POST: http://localhost:3000/users = Criar um usuário
+// GET: http://localhost:3000/users = Listar usuários
+// POST: http://localhost:3000/users/5 = Buscar dados do usuário com ID 5
+
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vemna própria rota geralmente opcionais para filtros, paginação
+
+const users = [
+    'Diego', // 0
+    'Cleiton', // 1
+    'Robson', // 2
+    'Roberto' // 3
+];
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+    const filteredUser = search ? users.filter(user => user.includes(search)) : users;
+    
+    return response.json(filteredUser);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response) => {
+    const user = {
+        name: 'Roberto Nicoletti',
+        email: 'roberto.urias@gmail.com'
+    };
+
+    return response.json(user);
+});
 
 app.listen(3333);
