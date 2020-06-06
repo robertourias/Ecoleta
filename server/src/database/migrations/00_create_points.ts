74de4e67d0d1a9898d3283f92cc552db46d68ce4
Roberto Nicoletti <roberto.urias@gmail.com>
2020-06-05 23:32:00 -0300

    get items, post points

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -14,6 +14,7 @@ export async function up(knex: Knex) {
  })
 }
 
+
 export async function down(knex: Knex) {
     return knex.schema.dropTable('points');
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,53 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json();
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+    const serializedItems = items.map(item => {
+        return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://localhost:3333/uploads/${item.image}`,
+        }
+    });
+    return response.json(serializedItems);
+});
+
+routes.post('/points', async (request, response) => {
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const ids = await knex('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf
+    });
+
+    const pointItems = items.map((item_id: number) =>{
+        return {
+            items_id: item_id,
+            points_id: ids[0]
+        }
+    });
+
+    await knex('points_items').insert(pointItems);
+
+    return response.json({ sucess: true });
 });
 
 export default routes;
